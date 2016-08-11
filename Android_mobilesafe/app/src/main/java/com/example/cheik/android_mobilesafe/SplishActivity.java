38fbdfa9ac84eccae64e7654c23092b2f68522bd
cxh1440578160 <1440578160@qq.com>
2016-08-11 21:13:05 +0800

    项目创建

---
+++ Android_mobilesafe/app/src/main/java/com/example/cheik/android_mobilesafe/SplishActivity.java
@@ -0,0 +1,13 @@
+package com.example.cheik.android_mobilesafe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splish);
+    }
+}
